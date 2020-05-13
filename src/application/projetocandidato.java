28339fdc1025e678af8d273276b208bd00d644da
Bruna Cardoso <bruna.carolinepc@gmail.com>
2020-05-13 20:16:41 -0300

    Leitura de escolaridade

--- src/application/projetocandidato.java
+++ src/application/projetocandidato.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class projetocandidato {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int escolaridade;
+		
+		// Leitura da escolaridade
+		
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.println("Digite uma opção: ");
+		
+		escolaridade = sc.nextInt();
+		
+		sc.close();
 
 	}
 
